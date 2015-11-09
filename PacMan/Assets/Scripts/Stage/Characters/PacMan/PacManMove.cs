2a0313663decce9afbabdfa4c813e3bf6def4ad1
GelianMaster <gelian92@mail.ru>
2015-11-09 16:49:43 +0100

    Called script Pacman
    Satte tillbaka fungerande movement på Pacman. Håller nu på att fixa rotation också

--- PacMan/Assets/Scripts/Stage/Characters/PacMan/PacManMove.cs
+++ PacMan/Assets/Scripts/Stage/Characters/PacMan/PacManMove.cs
@@ -1,44 +1,79 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class PacManMove : MonoBehaviour 
+public class Pacman : MonoBehaviour
 {
-	public float speed = 5.0f;
-	public Transform spawnPoint;
-	private AudioSource source;
-	public AudioClip death;
-	
-	
-	// Use this for initialization
-	void Start () 
-	{
-		source = GetComponent<AudioSource>();
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		Movement();
-	}
-	
-	void Movement ()
-	{
-		transform.Translate (Vector3.forward * Time.deltaTime * speed);
-		
-		if (Input.GetKeyDown(KeyCode.LeftArrow)|| Input.GetKeyDown(KeyCode.A))
-			transform.rotation *= Quaternion.Euler(0, -90, 0);
-		
-		if (Input.GetKeyDown(KeyCode.RightArrow)|| Input.GetKeyDown(KeyCode.D))
-			transform.rotation *= Quaternion.Euler(0, 90, 0);
-		
-		if (Input.GetKeyDown(KeyCode.DownArrow)|| Input.GetKeyDown(KeyCode.S))
-			transform.rotation *= Quaternion.Euler(0, 180, 0);
-	}
-	
-	void OnCollisionEnter (Collision col)
-	{
-		if(col.gameObject.tag == "Enemy")
-			gameObject.transform.position = spawnPoint.transform.position;
-		source.PlayOneShot (death,1f);
-	}
+	bool rightMovement = false;
+    bool leftMovement = false;
+    bool forwardMovement = false;
+    bool backMovement = false;
+    public float speed = 5.0f;
+    public Transform spawnPoint;
+    public AudioClip death;
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        AllWrong();
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            rightMovement = true;
+            leftMovement = false;
+            forwardMovement = false;
+            backMovement = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            leftMovement = true;
+            rightMovement = false;
+            forwardMovement = false;
+            backMovement = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            forwardMovement = true;
+            rightMovement = false;
+            leftMovement = false;
+            backMovement = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            backMovement = true;
+            rightMovement = false;
+            forwardMovement = false;
+            leftMovement = false;
+        }
+    }
+
+    void AllWrong()
+    {
+        if (forwardMovement == true)
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        if (backMovement == true)
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+
+        if (leftMovement == true)
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        if (rightMovement == true)
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Cube")
+            source.PlayOneShot(death, 1f);
+            gameObject.transform.position = spawnPoint.transform.position;
+    }
 }
