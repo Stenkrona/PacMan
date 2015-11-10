0413de4ec0a4a6f3f2f342ec654133dc3e136220
Marcus Stillberger <m.stillberger@gmail.com>
2015-11-10 23:22:57 +0100

    Navmesh, basic ghost movement
    som titeln säger

---
+++ PacMan/Assets/Scripts/Stage/Characters/Ghosts/GhostMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostMovement : MonoBehaviour {
+    float distanceToPacman;
+    
+    public Transform pacManPos;    
+    public Transform ghost;
+    public Transform spawnPoint;
+    public Transform[] points;
+
+    private int destPoint = 0;
+    NavMeshAgent ghostAgent;
+    int killCount = 0;
+    // Use this for initialization
+
+    void Start () {
+        ghostAgent = GetComponent<NavMeshAgent>();
+        ghostAgent.autoBraking = false;
+        Patrol();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        distanceToPacman = Vector3.Distance(ghost.position, pacManPos.position);
+        if (distanceToPacman < 7.0f) {
+            ghostAgent.destination = pacManPos.position;
+        }
+        else if (ghostAgent.remainingDistance < 0.5f)
+        {
+            Patrol();
+        }
+            
+    }
+
+    public void RespawnGhosts() {
+        // ghostAgent = GetComponent<NavMeshAgent>();
+        ghost.position = spawnPoint.position;
+
+    }
+
+
+    void Patrol()
+    {
+        // Set the agent to go to the currently selected destination.
+        ghostAgent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+}
---
+++ PacMan/Assets/Scripts/Stage/Characters/PacMan/Pacman.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using System.Collections;
+
+public class Pacman: MonoBehaviour
+{
+	bool rightMovement = false;
+    bool leftMovement = false;
+    bool forwardMovement = true;
+    bool backMovement = false;
+    public float speed = 5.0f;
+    public Transform spawnPoint;
+    public AudioClip death;
+    private AudioSource source;
+    private AudioSource sourci;
+    public AudioClip move;
+    bool playSound = true;
+    GameObject reSpawner;
+
+    void Start()
+    {
+        reSpawner = GameObject.FindGameObjectWithTag("Enemy");
+        source = GetComponent<AudioSource>();
+        sourci = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        //To do list on bool true
+        IfTrue();
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            transform.rotation = Quaternion.AngleAxis(90, Vector3.right);
+            rightMovement = true;
+            leftMovement = false;
+            forwardMovement = false;
+            backMovement = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            transform.rotation = Quaternion.AngleAxis(-90, Vector3.left);
+            leftMovement = true;
+            rightMovement = false;
+            forwardMovement = false;
+            backMovement = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            transform.rotation = Quaternion.AngleAxis(180, Vector3.forward);
+            forwardMovement = true;
+            rightMovement = false;
+            leftMovement = false;
+            backMovement = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            transform.rotation = Quaternion.AngleAxis(-180, Vector3.back);
+            backMovement = true;
+            rightMovement = false;
+            forwardMovement = false;
+            leftMovement = false;
+        }
+    }
+
+    //All movement false
+       void AllFalse ()
+        {
+            forwardMovement = false;
+            backMovement = false;
+            leftMovement = false;
+            rightMovement = false;
+        }
+
+        //This is a to do list
+        void IfTrue()
+        {
+            if (forwardMovement == true)
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+            if (backMovement == true)
+                transform.Translate(Vector3.back * Time.deltaTime * speed);
+
+            if (leftMovement == true)
+                transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+            if (rightMovement == true)
+                transform.Translate(Vector3.right * Time.deltaTime * speed);
+
+            if (playSound == true)
+                sourci.PlayOneShot(move, 1f);
+    }
+
+        //Teleportation och play sound on Enemy collide
+        void OnCollisionEnter(Collision col)
+        {
+        if (col.gameObject.tag == "Enemy")
+        {
+            gameObject.transform.position = spawnPoint.transform.position;
+            source.PlayOneShot(death, 1f);
+            AllFalse();
+            sourci.mute = !sourci.mute;
+            reSpawner.GetComponentInChildren<GhostMovement>().RespawnGhosts();
+        }
+        }
+}
